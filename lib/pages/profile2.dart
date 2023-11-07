ce09032237b793293b57c6059ee2bcc540a446c4
Iago Aurichio <iagoaurichio39@gmail.com>
2023-11-07 15:48:14 -0300

    finalizando tela profile2
    Signed-off-by: Iago Aurichio <iagoaurichio39@gmail.com>

--- lib/pages/profile2.dart
+++ lib/pages/profile2.dart
@@ -40,7 +40,13 @@ class Profile2 extends StatefulWidget {
 
 class _Profile2State extends State<Profile2> {
   SingingCharacter? _character = SingingCharacter.t1;
-
+  bool _showButton = true;
+  bool? _isCheckedProvas = false;
+  bool? _isCheckedTarefas = false;
+  bool? _isCheckedEventos = false;
+  bool? _isCheckedMensagens = false;
+  bool _selectPhotoProfile = false;
+  String? textButtonProfile = "Selecione uma foto de perfil";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -231,59 +237,280 @@ class _Profile2State extends State<Profile2> {
                           fontFamily: 'Lato-Regular',
                           fontSize: 18),
                     ))),
-            ElevatedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(const Color(0xff001E3B)),
-                    shadowColor: MaterialStateProperty.all(Colors.transparent),
-                    mouseCursor: MaterialStateMouseCursor.textable),
-                child: const Align(
+            _showButton
+                ? ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _showButton = false;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xff001E3B)),
+                      shadowColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: const Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                            padding: EdgeInsets.only(left: 10, top: 10),
+                            child: Text(
+                              'Selecione o curso, série e período antes',
+                              style: TextStyle(color: Color(0xffFF0000)),
+                            ))))
+                : Row(children: [
+                    Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 10),
+                        child: Radio(
+                          value: SingingCharacter.t1,
+                          groupValue: _character,
+                          onChanged: (SingingCharacter? value) {
+                            setState(() {
+                              _character = value;
+                            });
+                          },
+                          fillColor: MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.selected)) {
+                              return const Color(0xff1578D4);
+                            } else {
+                              return Colors.white;
+                            }
+                          }),
+                        )),
+                    const Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child:
+                            Text("T1", style: TextStyle(color: Colors.white))),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Radio(
+                        value: SingingCharacter.t2,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                      ),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child:
+                            Text("T2", style: TextStyle(color: Colors.white))),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Radio(
+                        value: SingingCharacter.t3,
+                        groupValue: _character,
+                        onChanged: (SingingCharacter? value) {
+                          setState(() {
+                            _character = value;
+                          });
+                        },
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                      ),
+                    ),
+                    const Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child:
+                            Text("T3", style: TextStyle(color: Colors.white))),
+                  ]),
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 20, bottom: 5),
+                child: Align(
                     alignment: Alignment.topLeft,
-                    child: Padding(
-                        padding: EdgeInsets.only(left: 15, top: 20),
-                        child: Text(
-                          'Selecione o curso, série e período antes',
-                          style: TextStyle(color: Color(0xffFF0000)),
-                        )))),
-            ListTile(
-                textColor: Colors.white,         
-                title: const Text('T1'),
-                leading: Radio<SingingCharacter>(
-                  fillColor: MaterialStateProperty.all(Colors.white),
-                  value: SingingCharacter.t1,
-                  groupValue: _character,
-                  onChanged: (SingingCharacter? value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
+                    child: Text(
+                      "Notificações:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            Row(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Checkbox(
+                        checkColor: const Color(0xff1578D4),
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                        value: _isCheckedProvas,
+                        onChanged: (value) {
+                          setState(() {
+                            _isCheckedProvas = value;
+                          });
+                        })),
+                const Text(
+                  "Provas",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Checkbox(
+                        checkColor: const Color(0xff1578D4),
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                        value: _isCheckedTarefas,
+                        onChanged: (value) {
+                          setState(() {
+                            _isCheckedTarefas = value;
+                          });
+                        })),
+                const Text(
+                  "Tarefas",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Checkbox(
+                        checkColor: const Color(0xff1578D4),
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                        value: _isCheckedEventos,
+                        onChanged: (value) {
+                          setState(() {
+                            _isCheckedEventos = value;
+                          });
+                        })),
+                const Text(
+                  "Eventos",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Checkbox(
+                        checkColor: const Color(0xff1578D4),
+                        fillColor: MaterialStateProperty.resolveWith<Color>(
+                            (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.selected)) {
+                            return const Color(0xff1578D4);
+                          } else {
+                            return Colors.white;
+                          }
+                        }),
+                        value: _isCheckedMensagens,
+                        onChanged: (value) {
+                          setState(() {
+                            _isCheckedMensagens = value;
+                          });
+                        })),
+                const Text(
+                  "Mensagens",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  width: 330,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectPhotoProfile = true;
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xff1578D4)),
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.only(top: 10.0, bottom: 10.0)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                    ),
+                    child: const Text(
+                      "Selecione uma foto de perfil",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Lato-Regular',
+                      ),
+                    ),
+                  ),
                 )),
-            ListTile(
-                textColor: Colors.white,
-                title: const Text('T2'),
-                leading: Radio<SingingCharacter>(
-                  fillColor: MaterialStateProperty.all(Colors.white),
-                  value: SingingCharacter.t2,
-                  groupValue: _character,
-                  onChanged: (SingingCharacter? value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                )),
-            ListTile(
-                textColor: Colors.white,
-                title: const Text('T3'),
-                leading: Radio<SingingCharacter>(
-                  fillColor: MaterialStateProperty.all(Colors.white),
-                  value: SingingCharacter.t3,
-                  groupValue: _character,
-                  onChanged: (SingingCharacter? value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 20, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Preview:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            _selectPhotoProfile
+                ? Image.asset("assets/images/profilePhoto.png")
+                : Image.asset("assets/images/preview.png", width: 270),
+            Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 20),
+                child: SizedBox(
+                  width: 230,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(const Color(0xff039000)),
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.only(top: 10.0, bottom: 10.0)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(13))),
+                    ),
+                    child: const Text(
+                      "Salvar",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Lato-Regular',
+                      ),
+                    ),
+                  ),
                 )),
           ],
         ),
