29edd2403f356d2ea2ce0004a8bda01cb7599aa4
Iago Aurichio <iagoaurichio39@gmail.com>
2023-11-07 10:41:53 -0300

    adicionando input radio na tela profile2
    Signed-off-by: Iago Aurichio <iagoaurichio39@gmail.com>

--- lib/pages/profile2.dart
+++ lib/pages/profile2.dart
@@ -29,6 +29,7 @@ final dropOpcoesPeriodo = [
   "Noturno / Noite"
 ];
 
+enum SingingCharacter { t1, t2, t3 }
 
 class Profile2 extends StatefulWidget {
   const Profile2({super.key});
@@ -38,6 +39,8 @@ class Profile2 extends StatefulWidget {
 }
 
 class _Profile2State extends State<Profile2> {
+  SingingCharacter? _character = SingingCharacter.t1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,162 +58,235 @@ class _Profile2State extends State<Profile2> {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Padding(
-              padding: EdgeInsets.only(left: 25, top: 50, bottom: 5),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Nome completo:",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Lato-Regular',
-                        fontSize: 18),
-                  ))),
-          const SizedBox(
-              width: 350,
-              height: 60,
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: "Digite seu nome",
-                    filled: true,
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(Radius.circular(15))),
-                    labelStyle: TextStyle(
-                        fontFamily: 'Lato-Regular',
-                        decoration: TextDecoration.none),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(color: Colors.black))),
-              )),
-          const Padding(
-              padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Curso:",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Lato-Regular',
-                        fontSize: 18),
-                  ))),
-          ValueListenableBuilder(
-              valueListenable: dropValueCurso,
-              builder: (BuildContext context, String value, _) {
-                return SizedBox(
-                  width: 350,
-                  height: 60,
-                  child: DropdownButtonFormField<String>(
-                    hint: const Text("Selecione o seu curso"),
-                    value: (value.isEmpty) ? null : value,
-                    onChanged: (escolha) =>
-                        dropValueCurso.value = escolha.toString(),
-                    items: dropOpcoesCurso
-                        .map(
-                          (op) => DropdownMenuItem(value: op, child: Text(op)),
-                        )
-                        .toList(),
-                    isExpanded: true,
-                    decoration: InputDecoration(
-                      focusedBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15))),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: const BorderSide(color: Colors.black)),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 50, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Nome completo:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            const SizedBox(
+                width: 350,
+                height: 60,
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Digite seu nome",
                       filled: true,
                       fillColor: Colors.white,
-                    ),
-                    iconSize: 26,
-                  ),
-                );
-              }),
-              const Padding(
-              padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Semestre/série:",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Lato-Regular',
-                        fontSize: 18),
-                  ))),
-          ValueListenableBuilder(
-              valueListenable: dropValueSerie,
-              builder: (BuildContext context, String value, _) {
-                return SizedBox(
-                  width: 350,
-                  height: 60,
-                  child: DropdownButtonFormField<String>(
-                    hint: const Text("Selecione"),
-                    value: (value.isEmpty) ? null : value,
-                    onChanged: (escolha) =>
-                        dropValueSerie.value = escolha.toString(),
-                    items: dropOpcoesSerie
-                        .map(
-                          (op) => DropdownMenuItem(value: op, child: Text(op)),
-                        )
-                        .toList(),
-                    isExpanded: true,
-                    decoration: InputDecoration(
-                      focusedBorder: const OutlineInputBorder(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
                           borderRadius: BorderRadius.all(Radius.circular(15))),
+                      labelStyle: TextStyle(
+                          fontFamily: 'Lato-Regular',
+                          decoration: TextDecoration.none),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: const BorderSide(color: Colors.black)),
-                      filled: true,
-                      fillColor: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          borderSide: BorderSide(color: Colors.black))),
+                )),
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Curso:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            ValueListenableBuilder(
+                valueListenable: dropValueCurso,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 350,
+                    height: 60,
+                    child: DropdownButtonFormField<String>(
+                      hint: const Text("Selecione o seu curso"),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValueCurso.value = escolha.toString(),
+                      items: dropOpcoesCurso
+                          .map(
+                            (op) =>
+                                DropdownMenuItem(value: op, child: Text(op)),
+                          )
+                          .toList(),
+                      isExpanded: true,
+                      decoration: InputDecoration(
+                        focusedBorder: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(color: Colors.black)),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      iconSize: 26,
                     ),
-                    iconSize: 26,
-                  ),
-                );
-              }),
-              const Padding(
-              padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Período:",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'Lato-Regular',
-                        fontSize: 18),
-                  ))),
-          ValueListenableBuilder(
-              valueListenable: dropValuePeriodo,
-              builder: (BuildContext context, String value, _) {
-                return SizedBox(
-                  width: 350,
-                  height: 60,
-                  child: DropdownButtonFormField<String>(
-                    hint: const Text("Selecione o período"),
-                    value: (value.isEmpty) ? null : value,
-                    onChanged: (escolha) =>
-                        dropValuePeriodo.value = escolha.toString(),
-                    items: dropOpcoesPeriodo
-                        .map(
-                          (op) => DropdownMenuItem(value: op, child: Text(op)),
-                        )
-                        .toList(),
-                    isExpanded: true,
-                    decoration: InputDecoration(
-                      focusedBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15))),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: const BorderSide(color: Colors.black)),
-                      filled: true,
-                      fillColor: Colors.white,
+                  );
+                }),
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Semestre/série:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            ValueListenableBuilder(
+                valueListenable: dropValueSerie,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 350,
+                    height: 60,
+                    child: DropdownButtonFormField<String>(
+                      hint: const Text("Selecione"),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValueSerie.value = escolha.toString(),
+                      items: dropOpcoesSerie
+                          .map(
+                            (op) =>
+                                DropdownMenuItem(value: op, child: Text(op)),
+                          )
+                          .toList(),
+                      isExpanded: true,
+                      decoration: InputDecoration(
+                        focusedBorder: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(color: Colors.black)),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      iconSize: 26,
                     ),
-                    iconSize: 26,
-                  ),
-                );
-              })
-        ],
+                  );
+                }),
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Período:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            ValueListenableBuilder(
+                valueListenable: dropValuePeriodo,
+                builder: (BuildContext context, String value, _) {
+                  return SizedBox(
+                    width: 350,
+                    height: 60,
+                    child: DropdownButtonFormField<String>(
+                      hint: const Text("Selecione o período"),
+                      value: (value.isEmpty) ? null : value,
+                      onChanged: (escolha) =>
+                          dropValuePeriodo.value = escolha.toString(),
+                      items: dropOpcoesPeriodo
+                          .map(
+                            (op) =>
+                                DropdownMenuItem(value: op, child: Text(op)),
+                          )
+                          .toList(),
+                      isExpanded: true,
+                      decoration: InputDecoration(
+                        focusedBorder: const OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15))),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: const BorderSide(color: Colors.black)),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      iconSize: 26,
+                    ),
+                  );
+                }),
+            const Padding(
+                padding: EdgeInsets.only(left: 25, top: 10, bottom: 5),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Turma:",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Lato-Regular',
+                          fontSize: 18),
+                    ))),
+            ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(const Color(0xff001E3B)),
+                    shadowColor: MaterialStateProperty.all(Colors.transparent),
+                    mouseCursor: MaterialStateMouseCursor.textable),
+                child: const Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                        padding: EdgeInsets.only(left: 15, top: 20),
+                        child: Text(
+                          'Selecione o curso, série e período antes',
+                          style: TextStyle(color: Color(0xffFF0000)),
+                        )))),
+            ListTile(
+                textColor: Colors.white,         
+                title: const Text('T1'),
+                leading: Radio<SingingCharacter>(
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  value: SingingCharacter.t1,
+                  groupValue: _character,
+                  onChanged: (SingingCharacter? value) {
+                    setState(() {
+                      _character = value;
+                    });
+                  },
+                )),
+            ListTile(
+                textColor: Colors.white,
+                title: const Text('T2'),
+                leading: Radio<SingingCharacter>(
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  value: SingingCharacter.t2,
+                  groupValue: _character,
+                  onChanged: (SingingCharacter? value) {
+                    setState(() {
+                      _character = value;
+                    });
+                  },
+                )),
+            ListTile(
+                textColor: Colors.white,
+                title: const Text('T3'),
+                leading: Radio<SingingCharacter>(
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  value: SingingCharacter.t3,
+                  groupValue: _character,
+                  onChanged: (SingingCharacter? value) {
+                    setState(() {
+                      _character = value;
+                    });
+                  },
+                )),
+          ],
+        ),
       ),
     );
   }
